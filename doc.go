dfe75aa5b0828579898860dc3660761bd4626dbf
Marcel van Lohuizen <mpvl@golang.org>
2017-08-22 14:07:35 +0200

    added package doc

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package textutil provides tools for creating Tranformers as used in
+// golang.org/x/text.
+package textutil
