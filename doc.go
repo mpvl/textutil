7dc5bb4d8efd75625800450e5dfd17b75633df8f
Marcel van Lohuizen <mpvl@golang.org>
2017-08-22 15:22:02 +0200

    fix typo

--- doc.go
+++ doc.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package textutil provides tools for creating Tranformers as used in
+// Package textutil provides tools for creating Transformers as used in
 // golang.org/x/text.
 package textutil
